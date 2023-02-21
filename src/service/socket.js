dc743ee92e57a08495edeafb3af3050e5b52338b
thechin press <thechinpress@gmail.com>
2023-02-21 21:40:45 +0630

    final commit

--- src/service/socket.js
+++ src/service/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://insta-clone-ui.netlify.app/");
+const socket = io("https://instagram-clone-bs7i.onrender.com/");
 
 export default socket;
